fdf4a37a58ea5f14bba5a0e873cca02c96a87f77
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-28 22:27:00 -0400

    change port to 3000 as 5000 is being used

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const morgan = require('morgan');
-const routes = require('./routes');
+const express = require('express')
+const morgan = require('morgan')
+const routes = require('./routes')
 
 // Create the Express app.
-const app = express();
+const app = express()
 
 // Setup request body JSON parsing.
-app.use(express.json());
+app.use(express.json())
 
 // Setup morgan which gives us HTTP request logging.
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 
 // Setup a friendly greeting for the root route.
 app.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to the REST API Validation with Express project!',
-  });
-});
+    message: 'Welcome to the REST API Validation with Express project!'
+  })
+})
 
 // Add routes.
-app.use('/api', routes);
+app.use('/api', routes)
 
 // Send 404 if no other route matched.
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Route Not Found',
-  });
-});
+    message: 'Route Not Found'
+  })
+})
 
 // Setup a global error handler.
 app.use((err, req, res, next) => {
-  console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+  console.error(`Global error handler: ${JSON.stringify(err.stack)}`)
 
   res.status(500).json({
     message: err.message,
-    error: process.env.NODE_ENV === 'production' ? {} : err,
-  });
-});
+    error: process.env.NODE_ENV === 'production' ? {} : err
+  })
+})
 
 // Set our port.
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000)
 
 // Start listening on our port.
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
-});
+  console.log(`Express server is listening on port ${server.address().port}`)
+})
