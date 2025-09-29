d1789bc0e291f158589fcaa2e16c159fd46e559f
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-28 22:38:48 -0400

    set up validation in routes.js

--- routes.js
+++ routes.js
@@ -1,29 +1,45 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
 // This array is used to keep track of user records
 // as they are created.
-const users = [];
+const users = []
 
 // Construct a router instance.
-const router = express.Router();
+const router = express.Router()
 
 // Route that returns a list of users.
 router.get('/users', (req, res) => {
-  res.json(users);
-});
+  res.json(users)
+})
 
 // Route that creates a new user.
 router.post('/users', (req, res) => {
   // Get the user from the request body.
-  const user = req.body;
+  const user = req.body
 
-  // Add the user to the `users` array.
-  users.push(user);
+  const errors = []
+
+  // Validate that we have a 'name' value
+  if (!user.name) {
+    errors.push('Please provide a value for "name".')
+  }
+
+  // Validate email
+  if (!user.email) {
+    errors.push('Please provide a value for "email".')
+  }
+
+  console.log(errors.length)
+  // Add the user to the `users` array if no errors.
+  if (errors.length > 0) {
+    // Return validation errors to the client
+    res.status(400).json({ errors })
+  }
 
   // Set the status to 201 Created and end the response.
-  res.status(201).end();
-});
+  res.status(201).end()
+})
 
-module.exports = router;
+module.exports = router
